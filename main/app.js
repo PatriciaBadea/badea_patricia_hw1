79941334b7975d645ef2d36bdea8a4cfe064776f
Patricia Badea <badeapatricia18@stud.ase.ro>
2020-11-01 17:02:30 +0200

    task completed for hw1

--- main/app.js
+++ main/app.js
@@ -1,6 +1,31 @@
-function removeOrderItem(orderInfo, position){
+function removeOrderItem(orderInfo, position) {
+
+    if (!Array.isArray(orderInfo.items)) {
+        throw 'Items should be an array'
+    } else if (position > orderInfo.items.length) {
+        throw 'Invalid position'
+    } 
+
+        for(var i=0; i< orderInfo.items.length; i++)
+        {
+            if(!(orderInfo.items[i].hasOwnProperty('price') && orderInfo.items[i].hasOwnProperty('quantity')))
+            throw 'Malformed item'
+        }
+        
+
+    orderInfo.items.splice(position,1)
+    
+    orderInfo.total = 0;
+    for(var i=0; i<orderInfo.items.length;i++)
+    {
+        orderInfo.total = orderInfo.total + orderInfo.items[i].price * orderInfo.items[i].quantity;
+    }
+
+   
+    return orderInfo;
     
 }
+
 
 const app = {
     removeOrderItem
